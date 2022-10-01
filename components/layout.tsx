180daa7e4f4b0ad7df9b8a9d70f7a028c12781ff
Nomad <nomad@Nomads-MacBook-Pro.local>
2022-10-01 13:02:48 -0400

    switch to antdesign, implement leftbar and header

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,33 +1,28 @@
-import Menu from './menu'
-import RightBar from './rightbar'
-import MainHeader from './main_header'
+import { Layout as AntLayout } from 'antd'
+import Left from './left'
+import Header from './header'
+
+const AntHeader = AntLayout.Header
+const { Footer, Sider, Content } = AntLayout
 
 export default function Layout({ children }: any) {
   return (
-    <div className='flex justify-between max-w-[1496px] my-0 mx-auto'>
-      <header className='hidden pl-4
-                        xs:block xs:flex-grow-0 xs:flex-shrink-0 xs:basis-16
-                        md:block md:flex-grow-1 md:flex-shrink-1 md:basis-0
-                        xl:block xl:flex-grow-0 xl:flex-shrink-0 xl:basis-[248px]'>
-        <a href='/' className='w-[123px] h-[63px] inline-block ml-2'>
-          <img src='1a667685a3d219cfd780ee3f0592a067.png' />
-        </a>
-        <Menu />
-      </header>
-      <main className='p-0 m-0
-                      xs:flex-grow-1 xs:flex-shrink-1 xs:basis-0
-                      sm:pl-0
-                      md:pl-4
-                      lg:w-[972px]
-                      xl:pl-8
-                      2xl:max-h-[1248px] 2xl:w-full 2xl:pl-[140px]'>
-        <MainHeader />
-        {/* main wrap */}
-        <div className='flex justify-between'>
-          {/* { children } */}
-          {/* <RightBar /> */}
-        </div>
-      </main>
-    </div>
+    <AntLayout className='max-w-[1496px] my-0 mx-auto'>
+      <Sider>
+        <Left />
+      </Sider>
+      <AntLayout>
+        <AntHeader className='bg-primary-bg flex justify-between items-center pl-0'>
+          <Header />
+        </AntHeader>
+        <AntLayout>
+          <Content>{children}</Content>
+          <Sider>
+            <Content>Sider</Content>
+            <Footer>Footer</Footer>
+          </Sider>
+        </AntLayout>
+      </AntLayout>
+    </AntLayout>
   )
 }
---
+++ components/left.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import type { MenuProps } from 'antd'
+import { Menu } from 'antd'
+import { HomeOutlined, TrophyOutlined, GlobalOutlined, FieldTimeOutlined, CommentOutlined } from '@ant-design/icons'
+
+type MenuItem = Required<MenuProps>['items'][number]
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem
+}
+
+const items: MenuProps['items'] = [
+  getItem('Home', '1', <HomeOutlined />),
+  getItem('Rankings', '2', <TrophyOutlined />),
+  getItem('Discover', '3', <GlobalOutlined />),
+  getItem('Moments', '4', <FieldTimeOutlined />),
+  getItem('Groups', '5', <CommentOutlined />),
+]
+
+export default function LeftBar() {
+  const onClick: MenuProps['onClick'] = e => {
+    console.log('click ', e)
+  }
+  
+  return (
+    <div className='bg-primary-bg pl-4'>
+      <a href='/' className='ml-2'>
+        <img src='1a667685a3d219cfd780ee3f0592a067.png' height={63} width={123} />
+      </a>
+      <Menu
+        onClick={onClick}
+        className='bg-primary-bg'
+        defaultSelectedKeys={['1']}
+        mode="inline"
+        items={items}
+      />
+    </div>
+  )
+}
